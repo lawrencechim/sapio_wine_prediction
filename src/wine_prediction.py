503b1a8509e35376227db5e954ec94d8a949be99
Lawrence Chim <lawrence.chim@Lawrences-MacBook-Pro.local>
2018-02-22 11:56:47 -0800

    final commit

--- src/wine_prediction.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier,export_graphviz
-plt.style.use("ggplot")
